88e1f323f19fe01271c24b6c0039667ee68055c2
arunagiri0111-pixel <arunagiri0111@gmail.com>
2026-03-22 22:39:18 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,22 @@
-const CACHE_NAME = 'opsmate-v2';
+var CACHE = 'opsmate-v1';
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json'
-      ]);
-    }).then(function() {
-      return self.skipWaiting();
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(['/', '/index.html', '/manifest.json']);
     })
   );
 });
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.filter(function(k) {
-          return k !== CACHE_NAME;
-        }).map(function(k) {
-          return caches.delete(k);
-        })
-      );
-    }).then(function() {
-      return self.clients.claim();
-    })
-  );
+self.addEventListener('activate', function(e) {
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(cached) {
-      return cached || fetch(event.request);
-    }).catch(function() {
-      return caches.match('/index.html');
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request);
     })
   );
 });
