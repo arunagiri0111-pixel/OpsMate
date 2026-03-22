646aff3dcc275584a31ef3026139193037b8d543
arunagiri0111-pixel <arunagiri0111@gmail.com>
2026-03-22 22:32:39 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,42 @@
 const CACHE_NAME = 'opsmate-v2';
 
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
-
-// Install
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json'
+      ]);
+    }).then(function() {
+      return self.skipWaiting();
+    })
   );
 });
 
-// Activate
-self.addEventListener('activate', event => {
+self.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(k => k !== CACHE_NAME)
-          .map(k => caches.delete(k))
-      )
-    ).then(() => self.clients.claim())
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) {
+          return k !== CACHE_NAME;
+        }).map(function(k) {
+          return caches.delete(k);
+        })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
   );
 });
 
-// Fetch
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(cached => cached || fetch(event.request))
-      .catch(() => caches.match('/index.html'))
+    caches.match(event.request).then(function(cached) {
+      return cached || fetch(event.request);
+    }).catch(function() {
+      return caches.match('/index.html');
+    })
   );
 });
 ```
